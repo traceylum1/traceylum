b6c698e0115802e25fbc96deab783ffdb00a5527
Tracey Lum <traceylum@gmail.com>
2024-02-17 16:08:30 -0800

    updated about page paragraph

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,8 +8,8 @@ export default function about() {
     <h2>Background</h2>
     <p>
       Born and raised in the East Bay. I worked abroad in Taiwan for 6 years teaching English to both 
-      children and professionals in cram schools and online schools, and wrote English textbooks and 
-      articles for language learners. I moved back to the Bay in 2022 and began my CS learning journey.
+      children and professionals. I also wrote English textbooks and articles for language learners. 
+      I moved back to the Bay in 2022 and began my CS learning journey.
     </p>
   </>
   );
